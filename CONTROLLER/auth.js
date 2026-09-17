b8b199b6a4e2863a06502d777ff319f0b51a47af
Noor Fatima <noor2239064@gmail.com>
2026-09-17 23:50:05 +0500

    added log in logic

--- CONTROLLER/auth.js
+++ CONTROLLER/auth.js
@@ -4,18 +4,25 @@ const Auth = require("../MODEL/auth");
 const signUp = async (req, res) => {
   try {
     const { fullname, email, username, password } = req.body;
-    const userSignup = await Auth.create({
-      fullname: fullname,
-      email: email,
-      username: username,
-      password: password,
-    });
-    res.status(201).json({
-      message: "User Signed Up",
-      user: userSignup,
-    });
-  } catch {
-    res.status(501).json({
+    const existingUser = await Auth.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists.",
+      });
+    } else {
+      const userSignup = await Auth.create({
+        fullname: fullname,
+        email: email,
+        username: username,
+        password: password,
+      });
+      return res.status(201).json({
+        message: "User Signed Up",
+        user: userSignup,
+      });
+    }
+  } catch (error) {
+    return res.status(501).json({
       message: "Error during sign in",
       error: error.message,
     });
@@ -23,7 +30,23 @@ const signUp = async (req, res) => {
 };
 
 const logIn = async (req, res) => {
-  res.send("Log In");
+  const { username, password } = req.body;
+  const existingUser = await Auth.findOne({ username });
+  if (!existingUser) {
+    return res.status(401).json({
+      message: "User doesn't exist. Login first.",
+    });
+  }
+  if (existingUser.password !== password) {
+    return res.status(400).json({
+      message: "Username or Password incorrect",
+    });
+  } else {
+    return res.status(201).json({
+      message: "User logged in successfully.",
+      user: existingUser,
+    });
+  }
 };
 
 module.exports = { signUp, logIn };
