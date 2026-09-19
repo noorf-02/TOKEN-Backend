144d8551c1f932851bdb1fc1b7a77d07adf149fc
Noor Fatima <noor2239064@gmail.com>
2026-09-19 23:13:50 +0500

    bcrypt logic added successfully

--- CONTROLLER/auth.js
+++ CONTROLLER/auth.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const Auth = require("../MODEL/auth");
+const bcrypt = require('bcryptjs')
 
 const signUp = async (req, res) => {
   try {
     const { fullname, email, username, password } = req.body;
     const existingUser = await Auth.findOne({ username });
+    const hashedPassword = await bcrypt.hash(password,10);
     if (existingUser) {
       return res.status(400).json({
         message: "User already exists.",
@@ -14,7 +16,7 @@ const signUp = async (req, res) => {
         fullname: fullname,
         email: email,
         username: username,
-        password: password,
+        password: hashedPassword,
       });
       return res.status(201).json({
         message: "User Signed Up",
@@ -32,12 +34,13 @@ const signUp = async (req, res) => {
 const logIn = async (req, res) => {
   const { username, password } = req.body;
   const existingUser = await Auth.findOne({ username });
+  const matchedPassword = await bcrypt.compare(password, existingUser.password);
   if (!existingUser) {
     return res.status(401).json({
       message: "User doesn't exist. Login first.",
     });
   }
-  if (existingUser.password !== password) {
+  if (!matchedPassword) {
     return res.status(400).json({
       message: "Username or Password incorrect",
     });
